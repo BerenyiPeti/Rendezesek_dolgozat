0e5c3c29a93bb6ccded94bb8cfd169dec4cb90c0
BerenyiPeti <berenyipeti01@gmail.com>
2023-04-18 11:07:16 +0200

    kiir

--- src/rendezesek_dolgozat/Rendezes.java
+++ src/rendezesek_dolgozat/Rendezes.java
@@ -1,17 +1,17 @@
 package rendezesek_dolgozat;
 
 public class Rendezes {
-    
-    private static int[] szamok = {5, 8, 24, 21, 2, 6, 1, 7, 23, 3};
-    private static int[] rendezetlen = {1,3,2};
-    
+
+    private static int[] szamok = {6, 4, 2, 1, 8, 7, 3, 9, 5, 10};
+    private static int[] rendezetlen = {1, 3, 2};
+
     public static void main(String[] args) {
         kiir(rendez(szamok));
         Teszt teszt1 = new Teszt();
         teszt1.rendezettE(rendez(szamok));
         //teszt1.rendezettE(rendezetlen);
     }
-    
+
     public static int[] rendez(int[] tomb) {
         do {
             for (int i = 0; i < tomb.length - 1; i++) {
@@ -22,10 +22,10 @@ public class Rendezes {
                 }
             }
         } while (!ellenoriz(tomb));
-        
+
         return tomb;
     }
-    
+
     public static boolean ellenoriz(int[] tomb) {
         boolean rendezett = true;
         for (int i = 0; i < tomb.length - 1; i++) {
@@ -33,14 +33,15 @@ public class Rendezes {
                 rendezett = false;
             }
         }
-        
+
         return rendezett;
     }
-    
+
     private static void kiir(int[] tomb) {
         for (int i = 0; i < tomb.length; i++) {
-            System.out.println(tomb[i]);
+            System.out.print(tomb[i] + " ");
         }
+        System.out.println("");
     }
-    
+
 }
